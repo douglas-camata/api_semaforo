8eefbdaada6b14046d0583e11c2859b8ecb8b512
Douglas Camata de Araujo <douglas.camata@gmail.com>
2024-12-20 09:44:08 -0300

    Criação do Swagger

--- app.py
+++ app.py
@@ -1,8 +1,35 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
+from flask_swagger_ui import get_swaggerui_blueprint
+import os
+import json
+
 app = Flask(__name__)
 CORS(app)
+
+### Swagger configuration ###
+SWAGGER_URL = '/docs'  # URL para acessar a documentação
+API_URL = '/swagger.json'  # Caminho para o arquivo JSON de especificação Swagger
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI URL
+    API_URL,  # Swagger arquivo JSON URL
+    config={  # Configurações adicionais do Swagger UI
+        'app_name': "API Semaforo"
+    }
+)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
+@app.route('/swagger.json')
+def swagger_json():
+    if os.path.exists("swagger_spec.json"):
+        with open("swagger_spec.json", 'r') as json_file:
+            swagger_spec = json.load(json_file)
+            return swagger_spec
+    else:
+        return {}
+
 
 projeto = {
     "tempo_vermelho_via_1": 2500,
